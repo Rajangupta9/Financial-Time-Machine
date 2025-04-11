e1607e68a17716f4e247810e980c4244503cf0c5
Tushar Wasake <tusharwasake@gmail.com>
2025-04-11 20:18:57 +0530

    database

--- serverSide/src/server.ts
+++ serverSide/src/server.ts
@@ -1,19 +1,21 @@
-import app from './app';
-import dotenv from 'dotenv';
-import logger from './utils/logger'
+import app from "./app";
+import dotenv from "dotenv";
+import logger from "./utils/logger";
+import { connectDataBase } from "./config/database";
 
 // Load environment variables
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 // Start the server
 app.listen(PORT, () => {
   logger.info(`Server is running on port ${PORT}`);
+  connectDataBase();
 });
 
 // Handle unhandled rejections
-process.on('unhandledRejection', (err: Error) => {
-  logger.error('Unhandled Rejection:', err);
+process.on("unhandledRejection", (err: Error) => {
+  logger.error("Unhandled Rejection:", err);
   process.exit(1);
 });
